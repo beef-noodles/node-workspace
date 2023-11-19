c75374a8860d2cbe83c2acdea5feebf446ea998d
guzhongren <guzhongren@live.cn>
2023-11-19 22:29:04 +0800

    docs(workspance): using 3rd deps

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -1,4 +1,9 @@
+import _ from 'lodash'
 import { add } from '@node-workspace/shared-lib';
 
-const result = add(2, 3);
+const someValues = _.filter([1,2,3,4,5],(value) => {
+    return [3,4].includes(value)
+})
+
+const result = add(someValues[0], someValues[1]);
 console.log('Result:', result);
